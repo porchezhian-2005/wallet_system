3ee35883e59a301e584c6fb349af6ff1b33334c6
Porchezhian <porchezhian2005@gmail.com>
2026-04-16 17:54:44 +0530

    addfeature: referral code generation

--- wallet_backend/src/auth/auth.module.ts
+++ wallet_backend/src/auth/auth.module.ts
@@ -3,9 +3,28 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_ACCESS_SECRET')!,
+        signOptions: {
+          expiresIn:
+            configService.get<string>('JWT_ACCESS_EXPIRY') as any,
+        },
+      }),
+    }),
+  ],
+
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
